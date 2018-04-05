5fa9e2bb9a38d55f9b6063c2589cbc8ea53c75e9
avakashyadav <38068580+avakashyadav@users.noreply.github.com>
2018-04-05 18:56:55 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+dirank(arr,n)
+{
+    for(i=0; i<n; i++)
+    {
+
+    }
+}
+
+int main()
+{
+    int n,i,j;
+    char arr[n];
+    string str;
+    cin>>str;
+    n=str.len;
+    for(i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+    return 0;
+}
